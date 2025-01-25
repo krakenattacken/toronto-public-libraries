7faca5d4ef7f58e9af2aa76c2236cf608f043982
krakenattacken <laurence.jang1@gmail.com>
2025-01-25 17:12:02 -0500

    add charts, popup styling

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 An interactive map with statistics about Toronto Public Library branches bewteen 2013 and 2023. 
 
+## To Do
+- [ ] Make point styling breakpoints dynamic
+- [ ] Legend. 
+
 ## Data Sources:
 All data from the Toronto Open Data Portal.
 - Toronto region boundary (https://open.toronto.ca/dataset/regional-municipal-boundary/) 
 - Library branch general information (https://open.toronto.ca/dataset/library-branch-general-information/)
 - Library visits (https://open.toronto.ca/dataset/library-visits/)
 - Library circulation (https://open.toronto.ca/dataset/library-circulation/)
-- Library card registrations (https://open.toronto.ca/dataset/library-card-registrations/)
+- Library card registrations (https://open.toronto.ca/dataset/library-card-registrations/) (haven't used this yet)
--- map.js
+++ map.js
@@ -1,28 +1,29 @@
 let currentYear = 2023;
 let currentProperty = 'visits';
-let librariesData = {}; 
+let librariesData = {};
+let chartInstance = null;
+let currentLibrary = '';
 let yearLabel = document.getElementById('year');
 yearLabel.textContent = currentYear;
 
 let map = new maplibregl.Map({
     container: 'map', 
-    style: "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json", 
+    style: "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json", // to change
     center: [-79.4432, 43.7132], 
     zoom: 10.5,
+    bearing: -17,
     maxBounds: [[-80.95, 42.79212], [-77.900, 44.246449]]
 });
 
-// Add controls
 map.addControl(new maplibregl.NavigationControl(), 'top-right');
 map.addControl(new maplibregl.ScaleControl({ unit: 'metric' }), 'bottom-right');
 
-// Popup
 const popup = document.getElementById('popup');
 const closeButton = document.getElementById('close-button');
 closeButton.addEventListener('click', function() {
     popup.classList.remove('show');
     const expanded = popup.classList.contains('show');
-    closeButton.setAttribute('aria-expanded', expanded);
+    closeButton.setAttribute('aria-expanded', expanded); // aria-expanded never gets updated...
 });
 
 d3.json('data/libraries.geojson', (err, data) => {
@@ -118,49 +119,53 @@ d3.json('data/libraries.geojson', (err, data) => {
               zoom: 17
           });
       
-          // popup info
-          const name = e.features[0].properties.BranchName;
-          const address = e.features[0].properties.Address;
-          const website = e.features[0].properties.Website;
-          const telephone = e.features[0].properties.Telephone;
-          const photo = e.features[0].properties.PhotoURL;
+          currentLibrary = e.features[0];
       
-          document.getElementById('branch-name').textContent = name;
-          document.getElementById('branch-address').textContent = address;
-          document.getElementById('branch-telephone').textContent = telephone;
-          const branchWebsite = document.getElementById('branch-website');
-          branchWebsite.href = website;
-          branchWebsite.textContent = website;
-          const branchPhoto = document.getElementById('branch-photo')
-          branchPhoto.src = photo;
-          branchPhoto.alt = "Exterior of " + name + " library";
+          document.getElementById('branch-name').textContent = currentLibrary.properties.BranchName;
+          document.getElementById('branch-address').textContent = currentLibrary.properties.Address;
+          document.getElementById('branch-telephone').textContent = currentLibrary.properties.Telephone;
+          document.getElementById('branch-website').href = currentLibrary.properties.Website;
+          document.getElementById('opening-year').textContent = currentLibrary.properties.PresentSiteYear;
+          document.getElementById('branch-features').textContent = getBranchFeatures();
+          document.getElementById('branch-workstations').textContent = currentLibrary.properties.Workstations;
+          document.getElementById('branch-neighbourhood').textContent = currentLibrary.properties.NBHDName;
+          const branchPhoto = document.getElementById('branch-photo');
+          branchPhoto.src = currentLibrary.properties.PhotoURL;
+          branchPhoto.alt = "Exterior of " + currentLibrary.properties.BranchName + " library";
       
           popup.classList.add('show');
+          generateChart(currentLibrary);
         });
 
-        updateLayer(`${currentProperty}_${currentYear}`);
+        updateLayer();
     });
 });
 
-// Update the layer based on the selected year
-function updateLayer(year) {
-    // will likely change null value styling... tdb
+// Update layer style based on filters
+function updateLayer() {
+    // will likely change null value styling... tbd
     map.setPaintProperty('libraries', 'circle-color', [
         "case",
         ['==', ['get', `${currentProperty}_${currentYear}`], null],
         "#333333ab",
         ["interpolate", ["linear"], ["get", `${currentProperty}_${currentYear}`],
         10000, "#7ac8ff",
-        1000000, "#001c71"]
-    ]);
+        1000000, "#001c71"]],
+        { duration: 500,
+          easing: 'linear'
+        }
+      );
 
     map.setPaintProperty('libraries', 'circle-radius', [
         "interpolate",
         ["linear"],
         ["get", `${currentProperty}_${currentYear}`],
         10000, 5,
-        1000000, 25
-    ]);
+        1000000, 25],
+        { duration: 500,
+          easing: 'linear'
+        }
+      );
 }
 
 // slider update
@@ -169,9 +174,81 @@ document.getElementById('slider').addEventListener('input', (e) => {
   currentYear = selectedYear;
   yearLabel.textContent = currentYear;
   updateLayer(`${currentProperty}_${currentYear}`);
+  generateChart(currentLibrary);
 });
-
+// dropdown update
 document.getElementById('property-dropdown').addEventListener('change', (e) => {
   currentProperty = e.target.value;
   updateLayer(`${currentProperty}_${currentYear}`);
-})
+  generateChart(currentLibrary);
+});
+
+function getChartTitle() {
+  return (currentProperty === 'visits') ? 'VISITS'
+  : (currentProperty === 'cards') ? 'CARD REGISTRATIONS'
+  : 'CIRCULATED MATERIALS';
+}
+
+function generateChart(library) {
+  const title = getChartTitle();
+
+  const years = Array.from({length: 11}, (_, i) => 2013 + i);
+  const data = years.map(year => { 
+    const value = library.properties[`${currentProperty}_${year}`];
+    return value !== undefined ? value : 0;
+  });
+
+  const yearIndex = years.indexOf(currentYear);
+  const barColours = years.map((_, index) => { return index === yearIndex ? '#006ddb' : '#006ddb57'; });
+
+  // is there a better place to put this
+  Chart.defaults.font.family = '"Open Sans", Arial, Helvetica, sans-serif';
+  Chart.defaults.color = '#000';
+  Chart.defaults.font.size = 10;
+
+  if (chartInstance) {
+    chartInstance.destroy();
+  }
+  chartInstance = new Chart(document.getElementById('chart'), {
+    type: 'bar',
+    options: {
+      indexAxis: 'y',
+      aspectRatio: 1.5,
+      scales: {
+        x: {
+          title: { display: true, text: `${title}`},
+        },
+        y: {
+          grid: { display: false }
+        }
+      },
+      plugins: {
+        legend: { display: false }
+      }
+    },
+    data: {
+      labels: years,
+      datasets: [
+        {
+          data: data,
+          backgroundColor: barColours
+        }
+      ]
+    }
+  });
+};
+
+function getBranchFeatures() {
+  const featureArray = [];  
+  if (currentLibrary.properties.KidsStop) { featureArray.push("Kids Stop"); }
+  if (currentLibrary.properties.LeadingReading) { featureArray.push("Leading To Reading"); }
+  if (currentLibrary.properties.CLC) { featureArray.push("Computer Learning Centre"); }
+  if (currentLibrary.properties.DIH) { featureArray.push("Digital Innovation Hub"); }
+  if (currentLibrary.properties.TeenCouncil) { featureArray.push("Teen Council"); }
+  if (currentLibrary.properties.YouthHub) { featureArray.push("Youth Hub"); }
+  if (currentLibrary.properties.AdultLiteracyProgram) { featureArray.push("Adult Literacy Program"); }
+
+  const features = featureArray.join(", ");
+  
+  return features;
+}
