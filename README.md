80b5682a6c49ebddeb302db98ec8fec66fdef5e3
krakenattacken <laurence.jang1@gmail.com>
2025-01-26 12:36:59 -0500

    add dynamic breakpoint styling, change colours to hsl

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Toronto Public Library Map
 
-An interactive map with statistics about Toronto Public Library branches bewteen 2013 and 2023. 
+An interactive map visualizing statistics about Toronto Public Library branches bewteen 2013 and 2023. 
 
 ## To Do
-- [ ] Make point styling breakpoints dynamic
+- [x] Make point styling breakpoints dynamic (currently jenks 5 classes)
 - [ ] Legend. 
+- [ ] Max point styling dynamic based on browser window size
+- [ ] Merge Osborne and Merril with Lillian H. Smith. 
+- [ ] Voronoi?
+- [ ] Add aggregation by sq. ft, population, etc.
+ 
+## Tools
+MapLibre GL JS, Chart.js, simple-statistics, D3. Made with vanilla JS, HTML, CSS. 
 
 ## Data Sources:
 All data from the Toronto Open Data Portal.
@@ -12,4 +19,6 @@ All data from the Toronto Open Data Portal.
 - Library branch general information (https://open.toronto.ca/dataset/library-branch-general-information/)
 - Library visits (https://open.toronto.ca/dataset/library-visits/)
 - Library circulation (https://open.toronto.ca/dataset/library-circulation/)
-- Library card registrations (https://open.toronto.ca/dataset/library-card-registrations/) (haven't used this yet)
+- Library card registrations (https://open.toronto.ca/dataset/library-card-registrations/)
+
+data/libraries.geojson joins all four of the library datasets, minus branches without coordinates. The fields cards_{year} correspond to card registrations, visit_{year} to visits, and circulation_{year} to circulation. 
--- map.js
+++ map.js
@@ -2,14 +2,24 @@ let currentYear = 2023;
 let currentProperty = 'visits';
 let librariesData = {};
 let chartInstance = null;
-let currentLibrary = '';
+let currentLibrary = null;
 let yearLabel = document.getElementById('year');
+let breaks = [];
 yearLabel.textContent = currentYear;
 
+const color1 = "hsl(205 100% 89%)";
+const color2 = "hsl(208 85.8% 75.1%)";
+const color3 = "hsl(208 81.9% 61%)";
+const color4 = "hsl(210 100% 42.9%)"; // this is the main blue.
+const color5 = "hsl(215 100% 32.5%)";
+const color6 = "hsl(225 100% 22.2%)";
+const strokeColor = "hsl(0 0% 100%)"; // white
+
+// Initialize the map
 let map = new maplibregl.Map({
-    container: 'map', 
+    container: 'map',
     style: "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json", // to change
-    center: [-79.4432, 43.7132], 
+    center: [-79.4432, 43.7132],
     zoom: 10.5,
     bearing: -17,
     maxBounds: [[-80.95, 42.79212], [-77.900, 44.246449]]
@@ -23,7 +33,9 @@ const closeButton = document.getElementById('close-button');
 closeButton.addEventListener('click', function() {
     popup.classList.remove('show');
     const expanded = popup.classList.contains('show');
-    closeButton.setAttribute('aria-expanded', expanded); // aria-expanded never gets updated...
+    closeButton.setAttribute('aria-expanded', expanded);
+    currentLibrary = null;
+    console.log(currentLibrary);
 });
 
 d3.json('data/libraries.geojson', (err, data) => {
@@ -59,113 +71,111 @@ d3.json('data/libraries.geojson', (err, data) => {
             source: 'notToronto',
             paint: {
                 'fill-color': '#beb9b1',
-                'fill-opacity': 0.5 
+                'fill-opacity': 0.5
             }
         });
-
-        // map.addSource('neighbourhoods', {
-        //     type: 'geojson',
-        //     data: 'data/toronto_neighbourhoods.geojson'
-        // });
-
-        // map.addLayer({
-        //     id: 'neighbourhoods',
-        //     type: 'fill',
-        //     source: 'neighbourhoods',
-        //     paint: {
-        //         'fill-color': "#3d3d3d",
-        //         'fill-opacity': 0.2
-        //     }
-        // });
 
         map.addSource('libraries', {
             type: 'geojson',
             data: librariesData
         });
 
+        breaks = getJenks();
         map.addLayer({
             id: 'libraries',
             type: 'circle',
             source: 'libraries',
             paint: {
-                'circle-color': ["interpolate",
-                    ["linear"],
-                    ["get", `${currentProperty}_${currentYear}`],
-                    10000, "#94d2ff",
-                    1000000, "#001c71"
+                'circle-color': [
+                  "step",
+                  ["get", `${currentProperty}_${currentYear}`],
+                  color1, breaks[0],
+                  color2, breaks[1],
+                  color3, breaks[2],
+                  color4, breaks[3],
+                  color5, breaks[4],
+                  color6
                 ],
-                'circle-stroke-color': '#fff',
+                'circle-stroke-color': strokeColor,
                 'circle-stroke-width': 1,
-                'circle-radius': ["interpolate",
-                    ["linear"],
-                    ["get", `${currentProperty}_${currentYear}`],
-                    10000, 5,
-                    1000000, 25
+                'circle-radius': [
+                  "step",
+                  ["get", `${currentProperty}_${currentYear}`],
+                  4, breaks[0],
+                  8, breaks[1],
+                  12, breaks[2],
+                  16, breaks[3],
+                  22, breaks[4],
+                  26
                 ]
             },
         });
-       
+
         map.on('mouseenter', 'libraries', () => {
-          map.getCanvas().style.cursor = 'pointer';
+            map.getCanvas().style.cursor = 'pointer';
         });
-      
+
         map.on('mouseleave', 'libraries', () => {
-          map.getCanvas().style.cursor = '';
+            map.getCanvas().style.cursor = '';
         });
-      
+
         map.on('click', 'libraries', (e) => {
-          map.flyTo({
-              center: e.features[0].geometry.coordinates,
-              zoom: 17
-          });
-      
-          currentLibrary = e.features[0];
-      
-          document.getElementById('branch-name').textContent = currentLibrary.properties.BranchName;
-          document.getElementById('branch-address').textContent = currentLibrary.properties.Address;
-          document.getElementById('branch-telephone').textContent = currentLibrary.properties.Telephone;
-          document.getElementById('branch-website').href = currentLibrary.properties.Website;
-          document.getElementById('opening-year').textContent = currentLibrary.properties.PresentSiteYear;
-          document.getElementById('branch-features').textContent = getBranchFeatures();
-          document.getElementById('branch-workstations').textContent = currentLibrary.properties.Workstations;
-          document.getElementById('branch-neighbourhood').textContent = currentLibrary.properties.NBHDName;
-          const branchPhoto = document.getElementById('branch-photo');
-          branchPhoto.src = currentLibrary.properties.PhotoURL;
-          branchPhoto.alt = "Exterior of " + currentLibrary.properties.BranchName + " library";
-      
-          popup.classList.add('show');
-          generateChart(currentLibrary);
+            map.flyTo({
+                center: e.features[0].geometry.coordinates,
+                zoom: 17
+            });
+
+            currentLibrary = e.features[0];
+
+            document.getElementById('branch-name').textContent = currentLibrary.properties.BranchName;
+            document.getElementById('branch-address').textContent = currentLibrary.properties.Address;
+            document.getElementById('branch-telephone').textContent = currentLibrary.properties.Telephone;
+            document.getElementById('branch-website').href = currentLibrary.properties.Website;
+            document.getElementById('opening-year').textContent = currentLibrary.properties.PresentSiteYear;
+            document.getElementById('branch-features').textContent = getBranchFeatures();
+            document.getElementById('branch-workstations').textContent = currentLibrary.properties.Workstations;
+            document.getElementById('branch-neighbourhood').textContent = currentLibrary.properties.NBHDName;
+            const branchPhoto = document.getElementById('branch-photo');
+            branchPhoto.src = currentLibrary.properties.PhotoURL;
+            branchPhoto.alt = "Exterior of " + currentLibrary.properties.BranchName + " library";
+
+            popup.classList.add('show');
+            generateChart();
         });
 
         updateLayer();
     });
 });
 
+function getJenks() {
+  const temp = librariesData.features.map(feature => feature.properties[`${currentProperty}_${currentYear}`] ?? 0);
+  return ss.jenks(temp, 5);
+}
+
 // Update layer style based on filters
 function updateLayer() {
-    // will likely change null value styling... tbd
+    breaks = getJenks();
     map.setPaintProperty('libraries', 'circle-color', [
-        "case",
-        ['==', ['get', `${currentProperty}_${currentYear}`], null],
-        "#333333ab",
-        ["interpolate", ["linear"], ["get", `${currentProperty}_${currentYear}`],
-        10000, "#7ac8ff",
-        1000000, "#001c71"]],
-        { duration: 500,
-          easing: 'linear'
-        }
-      );
+      "step",
+      ["get", `${currentProperty}_${currentYear}`],
+      color1, breaks[0],
+      color2, breaks[1],
+      color3, breaks[2],
+      color4, breaks[3],
+      color5, breaks[4],
+      color6
+    ]);
 
     map.setPaintProperty('libraries', 'circle-radius', [
-        "interpolate",
-        ["linear"],
-        ["get", `${currentProperty}_${currentYear}`],
-        10000, 5,
-        1000000, 25],
-        { duration: 500,
-          easing: 'linear'
-        }
-      );
+      "step",
+      ["get", `${currentProperty}_${currentYear}`],
+      4, breaks[0],
+      8, breaks[1],
+      12, breaks[2],
+      16, breaks[3],
+      22, breaks[4],
+      26
+    ]);
 }
 
 // slider update
@@ -174,13 +184,14 @@ document.getElementById('slider').addEventListener('input', (e) => {
   currentYear = selectedYear;
   yearLabel.textContent = currentYear;
   updateLayer(`${currentProperty}_${currentYear}`);
-  generateChart(currentLibrary);
+  if (currentLibrary !== null) generateChart(currentLibrary);
 });
+
 // dropdown update
 document.getElementById('property-dropdown').addEventListener('change', (e) => {
   currentProperty = e.target.value;
   updateLayer(`${currentProperty}_${currentYear}`);
-  generateChart(currentLibrary);
+  if (currentLibrary !== null) generateChart(currentLibrary);
 });
 
 function getChartTitle() {
@@ -189,19 +200,18 @@ function getChartTitle() {
   : 'CIRCULATED MATERIALS';
 }
 
-function generateChart(library) {
+function generateChart() {
   const title = getChartTitle();
 
   const years = Array.from({length: 11}, (_, i) => 2013 + i);
-  const data = years.map(year => { 
-    const value = library.properties[`${currentProperty}_${year}`];
-    return value !== undefined ? value : 0;
+  const data = years.map(year => {
+    return currentLibrary.properties[`${currentProperty}_${year}`] ?? 0;
   });
 
   const yearIndex = years.indexOf(currentYear);
-  const barColours = years.map((_, index) => { return index === yearIndex ? '#006ddb' : '#006ddb57'; });
+  const barColours = years.map((_, index) => { return index === yearIndex ? color4 : 'hsla(210 100% 42.9% / 0.34)'; });
 
-  // is there a better place to put this
+  // global styles
   Chart.defaults.font.family = '"Open Sans", Arial, Helvetica, sans-serif';
   Chart.defaults.color = '#000';
   Chart.defaults.font.size = 10;
@@ -239,7 +249,7 @@ function generateChart(library) {
 };
 
 function getBranchFeatures() {
-  const featureArray = [];  
+  const featureArray = [];
   if (currentLibrary.properties.KidsStop) { featureArray.push("Kids Stop"); }
   if (currentLibrary.properties.LeadingReading) { featureArray.push("Leading To Reading"); }
   if (currentLibrary.properties.CLC) { featureArray.push("Computer Learning Centre"); }
@@ -249,6 +259,5 @@ function getBranchFeatures() {
   if (currentLibrary.properties.AdultLiteracyProgram) { featureArray.push("Adult Literacy Program"); }
 
   const features = featureArray.join(", ");
-  
-  return features;
+  return (features === '') ? 'N/A' :features;
 }
